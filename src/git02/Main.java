bf2a41f112ae97f3c96eb95d43ee05245d51d91b
RyuuHou <22jy0140@jec.ac.jp>
2023-10-04 11:38:23 +0900

    氏名を追加

--- src/git02/Main.java
+++ src/git02/Main.java
@@ -5,6 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("日本電子専門学校!");
 		System.out.println("　　情報システム開発!");
+		System.out.println("　　1組");
+		System.out.println("　　日本　電子");
 	}
 
 }
