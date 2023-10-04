ceaaa5ba8fe518045a34b974e4941e3bdeffa076
RyuuHou <22jy0140@jec.ac.jp>
2023-10-04 11:12:06 +0900

    情報システム開発をインデント

--- src/git02/Main.java
+++ src/git02/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("日本電子専門学校!");
-		System.out.println("情報システム開発!");
+		System.out.println("　　情報システム開発!");
 	}
 
 }
